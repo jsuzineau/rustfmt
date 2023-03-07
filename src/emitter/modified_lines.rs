c852302751e5822d6b5eb469c749593b0712a7a1
Jean SUZINEAU <jean.suzineau@wanadoo.fr>
2023-03-07 15:58:09 +0100

    reformatage en KR pour comparaison avec master

--- src/emitter/modified_lines.rs
+++ src/emitter/modified_lines.rs
@@ -5,22 +5,20 @@ use std::io::Write;
 #[derive(Debug, Default)]
 pub(crate) struct ModifiedLinesEmitter;
 
-impl Emitter for ModifiedLinesEmitter
-{
-  fn emit_formatted_file(
-    &mut self,
-    output: &mut dyn Write,
-    FormattedFile {
-      original_text,
-      formatted_text,
-      ..
-    }: FormattedFile<'_>,
-  ) -> Result<EmitterResult, io::Error>
-  {
-    const CONTEXT_SIZE: usize = 0;
-    let mismatch = make_diff(original_text, formatted_text, CONTEXT_SIZE);
-    let has_diff = !mismatch.is_empty();
-    write!(output, "{}", ModifiedLines::from(mismatch))?;
-    Ok(EmitterResult { has_diff })
-  }
+impl Emitter for ModifiedLinesEmitter {
+    fn emit_formatted_file(
+        &mut self,
+        output: &mut dyn Write,
+        FormattedFile {
+            original_text,
+            formatted_text,
+            ..
+        }: FormattedFile<'_>,
+    ) -> Result<EmitterResult, io::Error> {
+        const CONTEXT_SIZE: usize = 0;
+        let mismatch = make_diff(original_text, formatted_text, CONTEXT_SIZE);
+        let has_diff = !mismatch.is_empty();
+        write!(output, "{}", ModifiedLines::from(mismatch))?;
+        Ok(EmitterResult { has_diff })
+    }
 }
--- src/emitter/stdout.rs
+++ src/emitter/stdout.rs
@@ -3,36 +3,30 @@ use crate::config::Verbosity;
 use std::io::Write;
 
 #[derive(Debug)]
-pub(crate) struct StdoutEmitter
-{
-  verbosity: Verbosity,
+pub(crate) struct StdoutEmitter {
+    verbosity: Verbosity,
 }
 
-impl StdoutEmitter
-{
-  pub(crate) fn new(verbosity: Verbosity) -> Self
-  {
-    Self { verbosity }
-  }
-}
-
-impl Emitter for StdoutEmitter
-{
-  fn emit_formatted_file(
-    &mut self,
-    output: &mut dyn Write,
-    FormattedFile {
-      filename,
-      formatted_text,
-      ..
-    }: FormattedFile<'_>,
-  ) -> Result<EmitterResult, io::Error>
-  {
-    if self.verbosity != Verbosity::Quiet
-    {
-      writeln!(output, "{}:\n", filename)?;
+impl StdoutEmitter {
+    pub(crate) fn new(verbosity: Verbosity) -> Self {
+        Self { verbosity }
     }
-    write!(output, "{}", formatted_text)?;
-    Ok(EmitterResult::default())
-  }
+}
+
+impl Emitter for StdoutEmitter {
+    fn emit_formatted_file(
+        &mut self,
+        output: &mut dyn Write,
+        FormattedFile {
+            filename,
+            formatted_text,
+            ..
+        }: FormattedFile<'_>,
+    ) -> Result<EmitterResult, io::Error> {
+        if self.verbosity != Verbosity::Quiet {
+            writeln!(output, "{}:\n", filename)?;
+        }
+        write!(output, "{}", formatted_text)?;
+        Ok(EmitterResult::default())
+    }
 }
